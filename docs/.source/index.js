34e5fc2a3f110e52ae227976d99bd8c3463efe20
Sadman Sakib <68962712+sadmann7@users.noreply.github.com>
2024-10-24 01:00:34 -0700

    chore: delete docs/.source directory

--- docs/.source/index.js
+++
@@ -1,5 +0,0 @@
-import { toRuntime, toRuntimeAsync } from "fumadocs-mdx"
-import * as docs_0 from "../content/docs/index.mdx?collection=docs&hash=08f3f9ac9126341bb766f2171b054d5b4697687796fbc937c054aa4811b4018d"
-import * as docs_1 from "../content/docs/test.mdx?collection=docs&hash=08f3f9ac9126341bb766f2171b054d5b4697687796fbc937c054aa4811b4018d"
-export const docs = [toRuntime("doc", docs_0, {"path":"docs\\index.mdx","absolutePath":"D:/Code/web/wfte/docs/content/docs/index.mdx"}), toRuntime("doc", docs_1, {"path":"docs\\test.mdx","absolutePath":"D:/Code/web/wfte/docs/content/docs/test.mdx"})];
-export const meta = [];

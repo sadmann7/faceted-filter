f1d485470d485eb39927cd64a1fce39730efa5d0
sadmann7 <sadmann898@gmail.com>
2024-10-24 16:29:30 +0600

    chore: empty

--- examples/nextjs/app/page.tsx
+++ examples/nextjs/app/page.tsx
@@ -1,28 +1,5 @@
 "use client";
 
-import { options } from "@/utils";
-import {
-  CreateOption,
-  Input,
-  MultiSelect,
-  Option,
-  Options,
-  SelectedOptions,
-} from "faceted-filter";
-
 export default function IndexPage() {
-  return (
-    <MultiSelect>
-      <Input placeholder="Select options..." />
-      <Options>
-        {options.map((option) => (
-          <Option key={option.value} value={option.value}>
-            {option.label}
-          </Option>
-        ))}
-      </Options>
-      <CreateOption />
-      <SelectedOptions />
-    </MultiSelect>
-  );
+  return <div>Faceted filter</div>;
 }
